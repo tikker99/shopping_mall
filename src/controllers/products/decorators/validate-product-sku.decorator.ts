07312fb33728cec6a6fc67e94cefbed989b3d4f7
tikkerobyan <tigran.kerobyan@mail.ru>
2024-07-03 13:10:44 +0400

    decorator fix

--- src/controllers/products/decorators/validate-product-sku.decorator.ts
+++ src/controllers/products/decorators/validate-product-sku.decorator.ts
@@ -7,7 +7,6 @@ import {
   ValidatorConstraintInterface
 } from 'class-validator';
 import { PrismaService } from 'src/services/prisma.service';
-import { CreateProductRequestBody } from 'src/controllers/products/schemas/products.request.dto';
 import { BadRequestException } from '@nestjs/common/exceptions/bad-request.exception';
 
 export function ValidateProductSku(validationOptions?: ValidationOptions) {
@@ -27,9 +26,11 @@ export class ProductSkuValidation implements ValidatorConstraintInterface {
   constructor(private prisma: PrismaService) {}
 
   async validate(value: string, validationArguments: ValidationArguments): Promise<boolean> {
-    const product = await this.prisma.product.findUnique({ where: { sku: value }});
-    if (product) {
-      throw new BadRequestException('Product with this SKU already exists');
+    if (value) {
+      const product = await this.prisma.product.findUnique({ where: { sku: value }});
+      if (product) {
+        throw new BadRequestException('Product with this SKU already exists');
+      }
     }
     return true;
   }
