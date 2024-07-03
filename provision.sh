b55983b53d697493ffe4d3b35ca1c371bffa7d6d
tikkerobyan <tigran.kerobyan@mail.ru>
2024-07-03 13:56:00 +0400

    provision.sh fix

--- provision.sh
+++ provision.sh
@@ -2,7 +2,6 @@ npm install
 
 docker-compose -f ./docker-compose.yml down
 docker volume rm shopping_mall_postgres_data --force
-docker volume rm shopping_mall_postgres_data --force
 
 docker-compose -f ./docker-compose.yml up -d
 sleep 5s
